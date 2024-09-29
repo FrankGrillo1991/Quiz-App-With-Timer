31dc7ec0496020e0d821167f856f0d368eb36b71
FrankGrillo1991 <fsg12491@aol.com>
2024-09-28 22:20:07 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+let questions = [
+    {
+        prompt: `Inside which HTML element do we put the JavaScript?`,
+        options: [
+            "<javascript>",
+            "<js>",
+            "<script>",
+            "<scripting>",
+        ],
+        answer: "<script>"
+    },
+
+    {
+        prompt: `How do you call a function named myFunction?` ,
+        options: [
+            "call myFunction()",
+            "myFunction()",
+            "call function myFunction",
+            "Call.myFunction",
+        ],
+        answer: "myFunction()"
+    },
+
+    {
+        prompt: `How does a for loop start?` ,
+        options: [
+            "for (i = 0; i <= 5; i++)",
+            "for (i = 0; i <= 5)",
+            "for i = 1 to 5",
+            " for (i <= 5; i++)",
+        ],
+        answer: "for (i = 0; i <= 5; i++)"
+    },
+
+    {
+        prompt: `In JavaScript, which of the following is the logical operator?` ,
+        options: ["|", "&&", "%", "/"],
+        answer: "&&"
+    },
+
+];
